ecc82f4b875d48f879f139f9576704a5e4d1286e
Cole Rau <colevrau@gmail.com>
2019-10-16 19:42:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)
   array.each_with_index { |item, index| p "#{index + 1}. #{item}." }
 end
 
-def summon_captain_planet
+def summon_captain_planet(array)
   # Your code here
 end
 
