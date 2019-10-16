cbe36a823e8070f264980b0af3126c79cc4f90dc
Cole Rau <colevrau@gmail.com>
2019-10-16 19:59:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.count >= 4
+  if array.count <= 4
     return true 
-  elsif array.count < 4 
+  elsif array.count > 4 
     return false 
   end 
 end
